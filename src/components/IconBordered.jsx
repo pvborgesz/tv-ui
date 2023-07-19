740ecdea5803a8df2e0bdb8e83d5c26a29152388
IA-V <iagoviper48@gmail.com>
2023-07-19 16:48:59 -0300

    Finalização da tela de importação de perfis

--- src/components/IconBordered.jsx
+++ src/components/IconBordered.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function IconBordered({ children, background }) {
   return (
-    <div className={`p-2 border border-white rounded-lg ${background}`}>
+    <div className={`hover:bg-zinc-700 hover:scale-105 transition-all duration-400 p-2 border border-white rounded-lg ${background}`}>
       {children}
     </div>
   );
