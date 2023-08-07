a8b2060779b7d75a836a0ec251de8094acd73262
Paulo Victor Borges <pvbo.lima@discente.ufma.br>
2023-08-07 01:33:51 -0300

    feat: add select profile

--- src/pages/SelectProfile.jsx
+++ src/pages/SelectProfile.jsx
@@ -7,7 +7,7 @@ import TitlePage from "../components/TitlePage";
 
 import { Link } from "react-router-dom";
 import ImageIconRounded from "../components/ImageIconRounded";
-
+import UserImg from '../assets/user-img.png'
 export default function SelectProfile() {
   const location = useLocation();
   const navigate = useNavigation();
@@ -21,7 +21,7 @@ export default function SelectProfile() {
 
   useEffect(() => {
 
-    linksRef.current[0].focus();
+    linksRef.current[3].focus();
 
     const handleKeyDown = (event) => {
       switch (event.key) {
@@ -81,37 +81,6 @@ export default function SelectProfile() {
       </header>
 
       <div className="p-4 rounded flex items-center justify-center text-white flex-grow my-10 mx-10 gap-8 mr-10 overflow-hidden">
-        <Link
-          className="flex flex-col items-center gap-10 cursor-pointer p-14 hover:bg-zinc-700 hover:scale-105 rounded-xl border-2 h-[370px] justify-center transition-all duration-400 focus:border-[10px]"
-          to={"/discoverChannels"}
-          ref={(el) => linksRef.current[0] = el}
-        >
-          <button className="flex flex-col items-center justify-center  cursor-pointer  rounded-sm ">
-            <ImageIconRounded icon={"https://github.com/pvborgesz.png"} />
-            <h4 className="text-xl pt-10 font-normal mb-1.5">Paulo Victor </h4>
-          </button>
-        </Link>
-        <Link
-          className="flex flex-col items-center gap-10 cursor-pointer p-14 hover:bg-zinc-700 hover:scale-105 rounded-xl border-2 h-[370px] justify-center transition-all duration-400 focus:border-[10px]"
-          to={"/discoverChannels"}
-          ref={(el) => linksRef.current[1] = el}
-        >
-          <div className="flex flex-col items-center justify-center  cursor-pointer  rounded-sm ">
-            <ImageIconRounded icon={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRph8E-hI-Gc9fPnGK8RY1LXsrDlzzsj6zli7qBy1aIT3aAZUkJ2X90qqJ6qVttu6iucPI&usqp=CAU"} />
-            <h4 className="text-xl pt-10 font-normal mb-1.5">Carlos de Salles</h4>
-          </div>
-        </Link>
-
-        <Link
-          className="flex flex-col items-center gap-10 cursor-pointer p-14 hover:bg-zinc-700 hover:scale-105 rounded-xl border-2 h-[370px] justify-center transition-all duration-400 focus:border-[10px]"
-          to={"/discoverChannels"}
-          ref={(el) => linksRef.current[2] = el}
-        >
-          <div className="flex flex-col items-center justify-center  cursor-pointer  rounded-sm ">
-            <ImageIconRounded icon={"https://files.sunoresearch.com.br/p/uploads/2021/12/Bill_Gates_.jpg"} />
-            <h4 className="text-xl pt-10 font-normal mb-1.5">Bill Gates</h4>
-          </div>
-        </Link>
 
         <Link
           className="flex flex-col items-center gap-10 cursor-pointer p-14 hover:bg-zinc-700 hover:scale-105 rounded-xl border-2 h-[370px] justify-center transition-all duration-400 focus:border-[10px]"
@@ -119,8 +88,8 @@ export default function SelectProfile() {
           ref={(el) => linksRef.current[3] = el}
         >
           <div className="flex flex-col items-center justify-center  cursor-pointer  rounded-sm ">
-            <ImageIconRounded icon={"https://cdn-icons-png.flaticon.com/512/3135/3135715.png"} />
-            <h4 className="text-xl pt-10 font-normal mb-1.5">Visitante</h4>
+            <ImageIconRounded icon={UserImg} />
+            <h4 className="text-2xl pt-10 font-normal mb-1.5 text-center ">Continuar sem criar <br />Perfil</h4>
           </div>
         </Link>
 
@@ -131,7 +100,7 @@ export default function SelectProfile() {
         >
           <div className="flex flex-col items-center justify-center  cursor-pointer  rounded-sm ">
             <ImageIconRounded icon={"https://d2gg9evh47fn9z.cloudfront.net/1600px_COLOURBOX5697474.jpg"} />
-            <h4 className="text-xl pt-10 font-normal mb-1.5">Adicionar Perfil</h4>
+            <h4 className="text-2xl pt-10 font-normal mb-1.5">Adicionar Perfil</h4>
           </div>
         </Link>
 
